a2f2c1dc5828818121d8cb0c4f4805f3f8238845
kamotedelight <kamotedelight@example.com>
2026-07-09 04:46:30 +0800

    chore: change capacitor config to JSON to resolve compiling issue on GHA

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.kamoted.app',
-  appName: 'kamoted',
-  webDir: 'dist'
-};
-
-export default config;
